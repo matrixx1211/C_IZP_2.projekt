96abced6705687f9392922e21a5cc23fd41d2818
matrixx1211 <matrixx1211@github.com>
2020-11-19 00:14:43 +0100

    added files

---
+++ sps.c
@@ -0,0 +1,15 @@
+/*                            IZP projekt č.2                                */
+/*                            Marek Bitomský                                 */
+/*                                 2020                                      */
+/*                                                                           */
+/*                                                                           */
+/*     Cílem projektu je vytvořit tabulkový procesor na příkazové řádce.     */
+
+//Hlavičkové soubory
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
